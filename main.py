ab6b447ec8513d2a9747b19915fd51e2e07bf349
Iroha <944647289@qq.com>
2020-08-20 16:10:37 +0800

    Create br EffNet

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from process_data import get_data_transforms, SIIM_ISIC
 import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
-import torchvision.models as models
 
 import torchvision
 import torchvision.transforms as transforms
@@ -14,11 +14,10 @@ import argparse
 
 from models import *
 from utils import progress_bar
-from process_data import get_data_transforms, SIIM_ISIC
 
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-parser.add_argument('--lr', default=0.25, type=float, help='learning rate')
+parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true',
                     help='resume from checkpoint')
 args = parser.parse_args()
@@ -33,36 +32,28 @@ transform_train, transform_test = get_data_transforms()
 
 trainset = SIIM_ISIC(transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=64, shuffle=True, num_workers=2)
+        trainset,
+        batch_size=4,
+        num_workers=2,
+        shuffle=True
+    )
 
 testset = SIIM_ISIC(train=False, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
-    testset, batch_size=128, shuffle=True, num_workers=2)
-
-classes = ('true', 'false')
+        testset,
+        batch_size=4,
+        num_workers=2,
+        shuffle=False
+    )
 
 # Model
 print('==> Building model..')
-# net = VGG('VGG19')
-# net = ResNet18()
-# net = PreActResNet18()
-# net = GoogLeNet()
-# net = DenseNet121()
-# net = ResNeXt29_2x64d()
-# net = MobileNet()
-# net = MobileNetV2()
-# net = DPN92()
-# net = ShuffleNetG2()
-# net = SENet18()
-# net = ShuffleNetV2(1)
-# net = EfficientNetB0()+
-# net = RegNetX_200MF()
-net = models.MobileNetV2(num_classes=2)
+net = EfficientNetB0()
 net = net.to(device)
-cudnn.benchmark = True
-# if device == 'cuda':
+# if device == 'cuda:2':
 #     net = torch.nn.DataParallel(net)
 #     cudnn.benchmark = True
+cudnn.benchmark = True
 
 # if args.resume:
 #     # Load checkpoint.
@@ -75,9 +66,7 @@ cudnn.benchmark = True
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=args.lr,
-                      momentum=0.9, weight_decay=3e-4)
-total_epochs = 50
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, int(total_epochs))
+                      momentum=0.9, weight_decay=5e-4)
 
 
 # Training
@@ -87,11 +76,10 @@ def train(epoch):
     train_loss = 0
     correct = 0
     total = 0
-    for batch_idx, (inputs, meta,  targets) in enumerate(trainloader):
-        inputs, targets = inputs.to(device), targets.to(device=device)
+    for batch_idx, (inputs, meta, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device=device, dtype=torch.int64)
         optimizer.zero_grad()
         outputs = net(inputs)
-
         loss = criterion(outputs, targets)
         loss.backward()
         optimizer.step()
@@ -112,9 +100,9 @@ def test(epoch):
     correct = 0
     total = 0
     with torch.no_grad():
-        for batch_idx, (inputs, meta,  targets) in enumerate(testloader):
-            inputs, targets = inputs.to(device), targets.to(device)
-
+        for batch_idx, (inputs, meta, targets) in enumerate(testloader):
+            inputs, targets = inputs.to(device), targets.to(device=device, dtype=torch.int64)
+            targets = targets.view(1 ,-1)[0]
             outputs = net(inputs)
             loss = criterion(outputs, targets)
 
@@ -141,8 +129,6 @@ def test(epoch):
         best_acc = acc
 
 
-for epoch in range(start_epoch, start_epoch+total_epochs):
-    scheduler.step()
-    print('epoch ', epoch, 'lr ', scheduler.get_lr()[0])
+for epoch in range(start_epoch, start_epoch+200):
     train(epoch)
     test(epoch)
