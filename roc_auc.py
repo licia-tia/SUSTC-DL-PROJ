ee012c29ead8ba0a09b40face46f2a326e4754ed
Iroha <944647289@qq.com>
2020-08-24 17:27:06 +0800

    update EffNet

--- roc_auc.py
+++ roc_auc.py
@@ -95,3 +95,41 @@ if __name__ == '__main__':
         if not os.path.exists('./roc_auc'):
             os.mkdir('./roc_auc')
         plt.savefig('./roc_auc/' + net)
+
+    # ensemble model
+    positive_scores_total = []
+    targets_total = []
+
+    with torch.no_grad():
+        for batch_idx, (inputs, meta, targets) in enumerate(testloader):
+            # inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+            scores = nn.functional.softmax(outputs, dim=1)
+
+            positive_scores = scores[:, 1]
+            targets_total.append(targets.to('cpu'))
+            positive_scores_total.append(positive_scores.to('cpu'))
+
+    targets_total = torch.cat(targets_total)
+    positive_scores_total = torch.cat(positive_scores_total)
+    # auc_score = roc_auc_score(targets_total, positive_scores_total)
+    # print("auc: ", auc_score)
+
+    y, x, _ = roc_curve(targets_total, positive_scores_total)
+    auc_score2 = auc(y, x)
+    print("auc: ", auc_score2)
+    plt.figure()
+    lw = 2
+    plt.plot(y, x, color='darkorange',
+             lw=lw, label='ROC curve (AUC score = %0.2f)' % auc_score2)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic for Melanoma Classification')
+    plt.legend(loc="lower right")
+    # plt.show()
+    if not os.path.exists('./roc_auc'):
+        os.mkdir('./roc_auc')
+    plt.savefig('./roc_auc/' + net)
