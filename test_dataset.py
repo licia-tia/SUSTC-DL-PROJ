f33c033c2159d86a2efc672be7afe8c812040d65
Shengran Hu <11711011@mail.sustech.edu.cn>
2020-08-24 15:32:36 +0800

    Update test_dataset.py

--- test_dataset.py
+++ test_dataset.py
@@ -8,46 +8,36 @@ device = 'cuda:2'
 
 
 def calculate_mean_std():
-    trainset = SIIM_ISIC(transform=transforms.Compose([
-        transforms.ToTensor(),
-    ]))
-    loader = torch.utils.data.DataLoader(
-        trainset,
-        batch_size=1,
-        num_workers=1,
-        shuffle=False
-    )
+    train_transform, valid_transform = get_data_transforms()
+    # trainset = SIIM_ISIC(transform=train_transform)
+    # loader = torch.utils.data.DataLoader(
+    #     trainset,
+    #     batch_size=4,
+    #     num_workers=4,
+    #     shuffle=False
+    # )
 
-    validset = SIIM_ISIC(train=False, transform=transforms.Compose([
-        transforms.ToTensor(),
-    ]))
+    validset = SIIM_ISIC(train=False, transform=valid_transform)
 
     validloader = torch.utils.data.DataLoader(
-        trainset,
-        batch_size=1,
-        num_workers=1,
-        shuffle=False
+        validset,
+        batch_size=4,
+        num_workers=4,
+        shuffle=True
     )
 
     mean = 0.
     std = 0.
     nb_samples = 0.
-    for inputs, meta, targets in loader:
-        data = inputs
-        batch_samples = data.size(0)
-        data = data.view(batch_samples, data.size(1), -1)
-        mean += data.mean(2).sum(0)
-        std += data.std(2).sum(0)
-        nb_samples += batch_samples
-        if nb_samples % 10 == 0:
-            print('step: ', nb_samples)
+    # for inputs, meta, targets in loader:
+    #     inputs, targets = inputs.to(device), targets.to(device)
+    #     nb_samples += 1
+    #     if nb_samples % 10 == 0:
+    #         print('step: ', nb_samples)
+
     for inputs, meta, targets in validloader:
-        data = inputs
-        batch_samples = data.size(0)
-        data = data.view(batch_samples, data.size(1), -1)
-        mean += data.mean(2).sum(0)
-        std += data.std(2).sum(0)
-        nb_samples += batch_samples
+        inputs, targets = inputs.to(device), targets.to(device)
+        nb_samples += 1
         if nb_samples % 10 == 0:
             print('step: ', nb_samples)
 
@@ -62,26 +52,32 @@ if __name__ == '__main__':
     trainloader = torch.utils.data.DataLoader(
         trainset, batch_size=4, shuffle=True, num_workers=2)
 
-    validset = SIIM_ISIC(train=False, transform=train_transform)
+    validset = SIIM_ISIC(train=False, transform=valid_transform)
     validloader = torch.utils.data.DataLoader(
         validset, batch_size=4, shuffle=True, num_workers=2)
 
+    testset = SIIM_ISIC(train=False, test=True, transform=valid_transform)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=16, shuffle=True, num_workers=16)
+
+
     # how transform example
-    image = Image.open('/home/group3/DataSet/Training_set/img_1.jpg')
-    ori_img = transforms.ToTensor()(image)
-    image = train_transform(image)
-    plt.imshow(image.permute(1, 2, 0))
-    plt.show()
-    plt.imshow(ori_img.permute(1, 2, 0))
-    plt.show()
+    # image = Image.open('/home/group3/DataSet/Training_set/img_1.jpg')
+    # ori_img = transforms.ToTensor()(image)
+    # image = train_transform(image)
+    # plt.imshow(image.permute(1, 2, 0))
+    # plt.show()
+    # plt.imshow(ori_img.permute(1, 2, 0))
+    # plt.show()
 
     # calculate mean_std
-    # calculate_mean_std()
+    #calculate_mean_std()
 
-    for batch_idx, (inputs, meta, targets) in enumerate(trainloader):
-        inputs, targets = inputs.to(device), targets.to(device)
+    for batch_idx, (inputs, meta) in enumerate(testloader):
+        inputs = inputs.to(device)
         sex = meta['sex']
         age = meta['age_approx']
         location = meta['anatom_site_general_challenge']
+        print(batch_idx)
         # training process
         pass
