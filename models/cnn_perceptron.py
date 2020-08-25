9f00265bdd69e33c4020b2b494068311d42eed43
Jiajun Yu <55095307+Yu709806736@users.noreply.github.com>
2020-08-25 16:54:57 +0800

    Update cnn_perceptron.py

--- models/cnn_perceptron.py
+++ models/cnn_perceptron.py
@@ -1,3 +1,8 @@
+"""
+@author: Jiajun Yu
+@contact: 11711102@mail.sustech.edu.cn
+"""
+
 import pdb
 import torch
 from torch import nn
@@ -7,10 +12,18 @@ hidden = '20'
 device = 'cuda:2' if torch.cuda.is_available() else 'cpu'
 
 class Net(nn.Module):
-    def __init__(self, input_dim, output_dim=2, meta_dim=3, hidden_layer=hidden):
+    def __init__(self, input_dim, output_dim=2, meta_dim=8, hidden_layer=hidden):
+        '''
+            initialize the CNN plus MLP model
+            @param: input_dim: the dimension of the input vector
+            @param: output_dim: optional, the dimension of the output
+            @param: meta_dim: optional, the dimension of the meta data
+            @param: hidden_layer: optional, the structure of hidden layer, a string value
+            @return: none
+        '''
         super().__init__()
-        layer_dims = [64, 'pool', 128, 'pool', 256, 256, 'pool', 512, 'pool', 1024, 'pool', 1280]
-        self.view_num = 1280
+        layer_dims = [64, 'pool', 128, 'pool', 256, 256, 'pool', 512, 'pool', 1024, 'pool', 1280]  # the layer of the CNN
+        self.view_num = 1280  # the view shape should be (-1, view_num)
         layers = []
         in_dim = input_dim
         for i in layer_dims:
@@ -23,7 +36,7 @@ class Net(nn.Module):
         model = nn.Linear(self.view_num, 2)
         nn.init.normal_(model.weight, 0, 0.01)
         nn.init.zeros_(model.bias)
-        self.cnn_linear = nn.Sequential(nn.Dropout(0.2), model)
+        self.cnn_linear = nn.Sequential(nn.Dropout(0.2), model)  # the linear part of CNN, using a Dropout model here
         perceptron_layers = []
         in_dim = meta_dim + 2
         for dim in hidden_layer.split():
@@ -37,17 +50,27 @@ class Net(nn.Module):
 
 
     def _make_layer(self, in_channel, out_channel, kernel_size=3, stride=2, padding=1):
+        '''
+            method to create layer of CNN
+            @param: in_channel: input dimension of the layer
+            @param: out_channel: output dimension of the layer
+            @params: kernel_size, stride, padding: hyperparameters of the Conv2d module in the layer
+            @return: the layer with a Conv2d module, a batch normalization module, and an activation module(ReLU)
+        '''
         layer = [nn.Conv2d(in_channel, out_channel, kernel_size=kernel_size, stride=stride, padding=padding),
                  nn.BatchNorm2d(out_channel), nn.ReLU()]
         return layer
         
     def forward(self, img, meta):
+        # first send the img to CNN
         out = self.cnn(img)
         out = out.view(-1, self.view_num)
         score = self.cnn_linear(out)
-        # pdb.set_trace()
+        # score sent out from CNN
+        # combine the score with meta data(onehot)
         score = torch.cat((score, meta), 1)
         # print(score.size(), meta.size())
         score = score.float()
+        # send the score with meta to MLP
         out = self.perceptron(score)
         return out
